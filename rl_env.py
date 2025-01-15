d2aa475d99c8268c816645c99afc8d2f5e28d88c
William 'William <wi1123.syd@gmail.com>
2025-01-15 22:58:19 +1100

    Add step method to BitEnv

--- rl_env.py
+++ rl_env.py
@@ -1,6 +1,7 @@
 """RL Environment Classes in Gymnasium"""
 
 from collections.abc import Callable
+from typing import Optional
 import numpy as np
 from scipy.optimize import minimize
 import gymnasium as gym
@@ -8,7 +9,9 @@ import gymnasium as gym
 class BitEnv(gym.Env):
     """Bit environment
 
-    - Observation/state space contains only '0' or '1'
+    - Observation is either '0' or '1'
+    - Action from agent is array where first element is either '0' or '1' and
+      second element is number of steps taken so far
     - Sample from state space with probability 'p' for '1'
     - 'p' can be constant or function with parameter 'n' (current agent step)
     - Returns award of +1 if sample matches agent's guess; -1 if mismatch
@@ -55,8 +58,32 @@ class BitEnv(gym.Env):
         self._p = p
         
         # Action and observation spaces, which only contain '1' and '0'
-        self.action_space = gym.spaces.Discrete(2)
+        self.action_space = gym.spaces.Box(
+            low=np.array([0, 0]), high=np.array([1, np.inf]), dtype=np.uint64
+        )
         self.observation_space = gym.spaces.Discrete(2)
+
+        self._obs = 0
+
+    def _sample_obs(self, n: int = 0):
+        if callable(p):
+            p = self._p(n)
+        else:
+            p = self._p
+        if self.np_random.random() < p:
+            self._obs = 1
+        else:
+            self._obs = 0
 
     def _get_info(self):
         return {"p type": self._p_type}
+    
+    def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
+        # Initialise seed and sample obs.
+        super().reset(seed=seed)
+        self._sample_obs()
+
+        return self._obs, self._get_info()
+
+    # def sample(self, action):
+        #
