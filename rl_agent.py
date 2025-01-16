3e080f7b4be4e6d6a9584cdd29058282bd7cef88
William 'William <wi1123.syd@gmail.com>
2025-01-16 22:27:50 +1100

    Add abstract RL agent class

---
+++ rl_agent.py
@@ -0,0 +1,42 @@
+"""RL Agents in Gymnasium"""
+
+from abc import ABCMeta, abstractmethod
+import gymnasium as gym
+
+class Agent(metaclass=ABCMeta):
+    """
+    Abstract template for an agent class
+    """
+
+    def __init__(self, env: gym.Env):
+        """
+        Initialise agent
+
+        Arg:
+            env: the training environment 
+        """
+        self.env = env
+        self.training_error = list()
+
+    @abstractmethod
+    def get_action(self, obs: int) -> int:
+        """
+        Given observation, get action
+
+        Arg:
+            obs: observation, '1' or '0'
+        
+        Returns action
+        """
+
+    @abstractmethod
+    def update(
+        self,
+        obs: int,
+        action: int,
+        reward: int,
+        terminated: bool,
+        next_obs: int
+    ):
+        """Update agent"""
+
