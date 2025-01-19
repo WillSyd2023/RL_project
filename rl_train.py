f397bf71d074b4011889451b1a39ebfa26776234
William 'William <wi1123.syd@gmail.com>
2025-01-19 15:07:31 +1100

    Moved visualisation function to rl_train.py

--- rl_train.py
+++ rl_train.py
@@ -1,9 +1,12 @@
+import numpy as np
+from scipy.signal import fftconvolve
+from matplotlib import pyplot as plt
 from tqdm import tqdm
 import gymnasium as gym
 from gymnasium.wrappers import RecordEpisodeStatistics, TimeLimit
 from rl_agent import Agent
 
-def rl_train(
+def train(
         env: gym.Env,
         agent: Agent,
         time_limit: int = 100_000,
@@ -46,3 +49,35 @@ def rl_train(
         agent.decay_epsilon()
 
     return (env, agent)
+
+def visualise_train(env: gym.Env, agent: Agent):
+    """Visualise training outcomes
+
+    Mostly taken from https://gymnasium.farama.org/introduction/train_agent/
+    in 19 Jan 2025
+
+    Args:
+        env: post-training gym environment
+        agent: post-training agent
+    """
+    fig, axs = plt.subplots(1, 3, figsize=(20, 8))
+
+    # np.convolve will compute the rolling mean for 100 episodes
+    
+    axs[0].plot(fftconvolve(env.return_queue, np.ones(100)))
+    axs[0].set_title("Episode Rewards")
+    axs[0].set_xlabel("Episode")
+    axs[0].set_ylabel("Reward")
+    
+    axs[1].plot(fftconvolve(env.length_queue, np.ones(100)))
+    axs[1].set_title("Episode Lengths")
+    axs[1].set_xlabel("Episode")
+    axs[1].set_ylabel("Length")
+    
+    axs[2].plot(fftconvolve(agent.training_error, np.ones(100)))
+    axs[2].set_title("Training Error")
+    axs[2].set_xlabel("Episode")
+    axs[2].set_ylabel("Temporal Difference")
+    
+    plt.tight_layout()
+    plt.show()
