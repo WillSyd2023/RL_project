08679bef2bb142ab3033d754851161a9ed341226
William 'William <wi1123.syd@gmail.com>
2025-03-17 22:46:58 +0700

    feat: initial draft for PolicyEvalQL, with initialisation for environment and agent

--- rl_policy_eval.py
+++ rl_policy_eval.py
@@ -1,14 +1,14 @@
+from collections import defaultdict
+import numpy as np
 import gymnasium as gym
 
-def policy_eval_ql(
-        env: gym.Env,
-        initial_epsilon: float,
-        epsilon_decay: float,
-        final_epsilon: float,
-        learning_rate: float = 0.01,
-        discount_factor: float = 0.95,
-    ):
-    """Policy-evaluation approach taken from Braun, 2021 on pp. 51 par. 4:
+# Self-written code
+from rl_env import BitEnv
+from rl_agent import QLAgent
+
+class PolicyEvalQL():
+    """
+    Object for policy evaluation taken from Braun, 2021 on pp. 51 par. 4:
 
     - Repeat the following independent trials:
         1. Train the agent
@@ -16,22 +16,35 @@ def policy_eval_ql(
         3. Use Q-values greedily on some episodes, get the average reward
         4. Repeat 1 to 3 several times
     - Get the median of average rewards (taken after the same number of steps)
-      from every trial
+        from every trial
     - Plot medians
 
     For now, restrict to the Q-learning agent
-
-    Args:
-        env: gym environment
-        learning_rate: The learning rate
-        initial_epsilon: The initial epsilon value
-        epsilon_decay: The decay for epsilon
-        final_epsilon: The final epsilon value
-        discount_factor: The discount factor for computing the Q-value
-        n_trials: number of trials
-        limit_trial: time limit (in steps) for a trial
-        per_trial: after how many steps in a trial should we do steps 2 and 3
-        n_eps: number of episodes
-        limit_eps: time limit (in steps) for an episode
     """
+    def __init__(
+        self,
+        env: gym.Env = BitEnv(),
+    ):
+        """
+        Initialises the default training agent and environment
 
+        Default agent parameters inspired by pp. 51 of Braun, 2021:
+        - learning rate is set to 0.01
+        - epsilon is constant, set to 0.1
+        - discount factor is 0.99
+        - Q-values are initialised optimistically by setting all values to 1.0001
+
+        Default environment is just BitEnv with p = 0.5
+        """
+        self.ql_agent = QLAgent(
+            env,
+            learning_rate=0.01,
+            initial_epsilon=0.1,
+            epsilon_decay=0,
+            final_epsilon=0.1,
+            discount_factor=0.99,
+        )
+
+        self.ql_agent.q_values = defaultdict(
+            lambda: np.ones(env.action_space.n) * 1.0001
+        )
