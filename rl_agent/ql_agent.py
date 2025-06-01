1c9b901aa1073665ec81be53866bd312ab38c7e7
William 'William <wi1123.syd@gmail.com>
2025-06-01 13:07:53 +0700

    feat: use reinforcement learning agents from previous BitEnv code

--- rl_agent/ql_agent.py
+++ rl_agent/ql_agent.py
@@ -1,46 +1,11 @@
-"""RL Agents in Gymnasium"""
+"""RL QL Agent in Gymnasium"""
 
-from abc import ABCMeta, abstractmethod
 from collections import defaultdict
 import copy
 import numpy as np
 import gymnasium as gym
 
-class Agent(metaclass=ABCMeta):
-    """
-    Abstract template for an agent class
-    """
-    def __init__(self, env: gym.Env):
-        """
-        Initialise agent
-
-        Arg:
-            env: the training environment 
-        """
-        self.env = env
-        self.training_error = []
-
-    @abstractmethod
-    def get_action(self, obs: int) -> int:
-        """
-        Given observation, get action
-
-        Arg:
-            obs: observation, '1' or '0'
-        
-        Returns action
-        """
-
-    @abstractmethod
-    def update(
-        self,
-        obs: int,
-        action: int,
-        reward: int,
-        terminated: bool,
-        next_obs: int
-    ):
-        """Update agent"""
+from parent_agent import Agent
 
 class QLAgent(Agent):
     """
