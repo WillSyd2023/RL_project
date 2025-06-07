2eb56c70f31703bed97ee7a0ad30468d6d46731e
William 'William <wi1123.syd@gmail.com>
2025-06-07 17:50:26 +0700

    feat: non-Markovian reward functions

--- nonmarkov_reward_problem/nonmarkov_rewards.py
+++ nonmarkov_reward_problem/nonmarkov_rewards.py
@@ -1,3 +1,31 @@
 """Non-Markovian rewards"""
 
-from nonmarkov_reward_problem.trace import TraceNode, TraceList
+from nonmarkov_reward_problem.trace import TraceList
+
+def reward_1(trace: TraceList) -> int:
+    """Reward for using arm 1 four consecutive steps, followed by arm 3"""
+    latest_actions = ["3", "1", "1", "1", "1"]
+    reward = 1
+    node = trace.head
+    for action in latest_actions:
+        if action in node.pi:
+            node = node.next
+        else:
+            reward = 0
+            break
+
+    return reward
+
+def reward_4(trace: TraceList) -> int:
+    """Reward for playing arm 3 twice in a row, then arm 2"""
+    latest_actions = ["2", "3", "3"]
+    reward = 1
+    node = trace.head
+    for action in latest_actions:
+        if action in node.pi:
+            node = node.next
+        else:
+            reward = 0
+            break
+
+    return reward
