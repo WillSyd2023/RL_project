dc838c1bba0a53b094950fe13f67af60be5ac809
William 'William <wi1123.syd@gmail.com>
2025-06-07 18:52:47 +0700

    feat, refactor: add Markovian rewards, and name accordingly

--- nmr_problem/rewards.py
+++ nmr_problem/rewards.py
@@ -1,9 +1,34 @@
-"""Non-Markovian rewards"""
+"""(Non-)Markovian rewards"""
 
 from nmr_problem.trace import TraceList
 
-def reward_1(trace: TraceList) -> int:
+def m_reward_1(trace: TraceList) -> int:
+    """Reward for using arm 3"""
+    if trace.size() < 1:
+        return 0
+
+    node = trace.head
+    if "3" in node.pi:
+        return 1
+    else:
+        return 0
+
+def m_reward_4(trace: TraceList) -> int:
+    """Reward for using arm 2"""
+    if trace.size() < 1:
+        return 0
+
+    node = trace.head
+    if "2" in node.pi:
+        return 1
+    else:
+        return 0
+
+def nm_reward_1(trace: TraceList) -> int:
     """Reward for using arm 1 four consecutive steps, followed by arm 3"""
+    if trace.size() < 5:
+        return 0
+
     latest_actions = ["3", "1", "1", "1", "1"]
     reward = 1
     node = trace.head
@@ -16,8 +41,11 @@ def reward_1(trace: TraceList) -> int:
 
     return reward
 
-def reward_4(trace: TraceList) -> int:
+def nm_reward_4(trace: TraceList) -> int:
     """Reward for playing arm 3 twice in a row, then arm 2"""
+    if trace.size() < 3:
+        return 0
+
     latest_actions = ["2", "3", "3"]
     reward = 1
     node = trace.head
