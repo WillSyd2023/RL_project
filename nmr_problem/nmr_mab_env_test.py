46de6e6bff52d2a2d34712b37f141937dbb40b1e
William 'William <wi1123.syd@gmail.com>
2025-07-20 14:46:02 +0700

    feat: more unit tests for nmr_problem

--- nmr_problem/nmr_mab_env_test.py
+++ nmr_problem/nmr_mab_env_test.py
@@ -1,49 +1,121 @@
 """(Non-)Markovian MAB environment test"""
 
-import pytest
+from copy import deepcopy
 from nmr_problem.nmr_mab_env import NonMarkovMABEnv
 from nmr_problem.rewards import m_reward_1, m_reward_4, nm_reward_1, nm_reward_4
 
-nm_env = NonMarkovMABEnv(rewards=[nm_reward_1, nm_reward_4])
-@pytest.mark.parametrize("trace_size, action, r", [
-    (1, 1, 0),
-    (2, 1, 0),
-    (3, 1, 0),
-    (4, 1, 0),
-    (5, 3, 10),
-    (6, 3, 0),
-    (7, 2, 1),
-])
-def test_steps_given_nm_rewards(trace_size, action, r):
+def test_reset():
+    """
+    Test environment reset behaviour
+    """
+    # Original environment and attributes
+    env = NonMarkovMABEnv(rewards=[nm_reward_1, m_reward_4])
+    ori_state = env.state
+    ori_actions = env.actions
+    ori_rewards = env.rewards
+
+    # Reset environment and get attributes
+    for action in [0, 3, 4, 2, 1, 4]:
+        _, _, _, _, _ = env.step(action)
+    state, trace = env.reset()
+    actions = env.actions
+    rewards = env.rewards
+
+    assert ori_state == state
+    assert ori_actions == actions
+    assert ori_rewards == rewards
+    assert trace.head is None
+    assert trace.size() == 0
+
+def test_deepcopy():
+    """
+    Test deepcopying behaviour
+    """
+    env = NonMarkovMABEnv(rewards=[nm_reward_1, m_reward_4])
+    for action in [0, 3, 4, 2, 1, 4]:
+        _, _, _, _, _ = env.step(action)
+    env_copy = deepcopy(env)
+
+    # Deepcopy copies actions
+    assert env_copy.actions == env.actions
+
+    # Deepcopy resets trace
+    assert env_copy.trace.head is None
+    assert env_copy.trace.size() == 0
+
+    # Deepcopy copies
+    assert env.rewards is not env_copy.rewards
+    for i, ori_reward in enumerate(env.rewards):
+        assert ori_reward is env_copy.rewards[i]
+
+def test_steps_given_nm_rewards():
     """Test step behavior of Non-Markovian environment
-    
+
+    Even when deepcopied
+
     Check outputs from sequential behaviour, after each step
     """
-    observation, terminated, truncated, reward, _ = nm_env.step(action)
+    nm_env = NonMarkovMABEnv(rewards=[nm_reward_1, nm_reward_4])
 
-    assert nm_env.trace.size() == trace_size
-    assert observation == "s"
-    assert terminated is False
-    assert truncated is False
-    assert reward == r
+    test_cases = [
+        (1, 1, 0),
+        (2, 1, 0),
+        (3, 1, 0),
+        (4, 1, 0),
+        (5, 3, 10),
+        (6, 3, 0),
+        (7, 2, 1),
+    ]
 
-nm_env_2 = NonMarkovMABEnv(rewards=[m_reward_1, m_reward_4])
-@pytest.mark.parametrize("trace_size, action, r", [
-    (1, 1, 0),
-    (2, 2, 1),
-    (3, 3, 100_000),
-    (4, 4, 0),
-    (5, 5, 0),
-])
-def test_steps_given_m_rewards(trace_size, action, r):
+    for trace_size, action, r in test_cases:
+        observation, terminated, truncated, reward, _ = nm_env.step(action)
+        assert nm_env.trace.size() == trace_size
+        assert observation == "s"
+        assert terminated is False
+        assert truncated is False
+        assert reward == r
+    
+    nm_env = deepcopy(nm_env)
+
+    for trace_size, action, r in test_cases:
+        observation, terminated, truncated, reward, _ = nm_env.step(action)
+        assert nm_env.trace.size() == trace_size
+        assert observation == "s"
+        assert terminated is False
+        assert truncated is False
+        assert reward == r
+
+def test_steps_given_m_rewards():
     """Test step behavior of Markovian environment
-    
+
+    Even when deepcopied
+
     Check outputs from sequential behaviour, after each step
     """
-    observation, terminated, truncated, reward, _ = nm_env_2.step(action)
+    m_env = NonMarkovMABEnv(rewards=[m_reward_1, m_reward_4])
 
-    assert nm_env_2.trace.size() == trace_size
-    assert observation == "s"
-    assert terminated is False
-    assert truncated is False
-    assert reward == r
+    test_cases = [
+        (1, 1, 0),
+        (2, 2, 1),
+        (3, 3, 100_000),
+        (4, 4, 0),
+        (5, 5, 0),
+    ]
+
+    for trace_size, action, r in test_cases:
+        observation, terminated, truncated, reward, _ = m_env.step(action)
+        assert m_env.trace.size() == trace_size
+        assert observation == "s"
+        assert terminated is False
+        assert truncated is False
+        assert reward == r
+    
+    m_env = deepcopy(m_env)
+
+    for trace_size, action, r in test_cases:
+        observation, terminated, truncated, reward, _ = m_env.step(action)
+        assert m_env.trace.size() == trace_size
+        assert observation == "s"
+        assert terminated is False
+        assert truncated is False
+        assert reward == r
